8e7c0671d25abfd30271113da8d3aba80ea418ce
Louis-Lorgere <louis.lorgere@gmail.com>
2022-03-17 19:07:50 +0100

    few modif  to navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
 import * as React from "react";
-import Navbar from "components/navbar";
 import Hero from "components/Hero";
-import ProductValues from "components/Products";
 import ProductCategories from "components/ProductCategories";
-import TeamHero from "components/TeamHero";
 import withRoot from "../withRoot";
 
 function Index() {
     return (
         <React.Fragment>
-            <Navbar/>
             <Hero/>
             <ProductCategories />
-            {/* <TeamHero /> */}
         </React.Fragment>
     );
 }
