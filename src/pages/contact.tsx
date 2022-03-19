ea5d6337cc6fb1416b295f245023efcac69804b1
FabCom <fabcom@communs.fr>
2022-03-19 16:01:02 +0100

    Add contact & 404 pages

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material"
+import Typography from "components/Typography"
+
+const Contact = () => {
+  return (
+    <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 10}}>
+      <Typography variant='h2'>Contactez-nous</Typography>
+      
+    </Box>
+  )
+}
+
+export default Contact
