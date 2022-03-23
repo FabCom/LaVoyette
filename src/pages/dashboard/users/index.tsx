6e7de0b08630ece373381842aedb50f80bd69f2f
FabCom <fabcom@communs.fr>
2022-03-23 19:42:51 +0100

    dashboard front harmonization

--- src/pages/dashboard/users/index.tsx
+++ src/pages/dashboard/users/index.tsx
@@ -2,12 +2,13 @@ import models from "lib/models"
 import { deserialize, serialize } from "superjson";
 import { User } from "@prisma/client"
 import { SuperJSONResult } from "superjson/dist/types"
-import { Button, Card, CardActions, CardContent, Chip } from "@mui/material"
+import { Button, Card, CardActions, CardContent, IconButton } from "@mui/material"
 import { Box } from "@mui/system"
 import Dashboard from "components/dashboard/LayoutDashboard"
 import Typography from "components/Typography"
 import Link from "next/link"
-import moment from 'moment';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 
 const UsersDashboard = ({ser_users}:{ser_users: SuperJSONResult}) => {
@@ -17,29 +18,29 @@ const UsersDashboard = ({ser_users}:{ser_users: SuperJSONResult}) => {
     <Dashboard >
       <Typography variant='h2' sx={{marginTop: 5}}>Utilisateurs</Typography>
       <Link href="/dashboard/stories/create" passHref><Button color="secondary" variant="contained" type="submit">Ajouter</Button></Link>
-      <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'space-between', flexWrap: 'wrap', padding:5, width: "100%"}}>
-          <Box sx={{ width: '100%', marginTop:5, padding:5 , display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-            <Box sx={{width:"60%"}}>
-            <span>Email</span>
-            </Box>
-            <Box sx={{width:"20%"}}>
-              <span>Rôle</span>
-            </Box>
-            <Box sx={{width:"20%"}}>
-            <span>Supprimer Éditer</span>
-            </Box>
+      <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'space-between', flexWrap: 'wrap', width: "100%"}}>
+        <Box sx={{ width: '100%', padding:5 , display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+          <Box sx={{width:"60%"}}>
+          <span>Titre</span>
           </Box>
+          <Box sx={{width:"30%"}}>
+            <span>Rôle</span>
+          </Box>
+          <Box sx={{width:"10%", display: 'flex', flexDirection: 'row', justifyContent: "space-between"}}>
+          <span>Supprimer</span> <span>Éditer</span>
+          </Box>
+        </Box>
         {users.map((user, i) => 
           <Card sx={{ marginBottom: 3 , display: 'flex'}} key={i}>
             <CardContent sx={{width:"60%"}}>
               <Typography variant='h5'>{user.email}</Typography>
             </CardContent>
-            <CardContent sx={{width:"20%"}}>
+            <CardContent sx={{width:"30%"}}>
               <Typography variant='h5'>{user.role}</Typography>
             </CardContent>
-            <CardActions sx={{width:"20%", justifyContent: 'center'}}>
-              <Link href={`/dashboard/users/${user.id}/delete`} passHref><Button variant="contained" size="small">Supprimer</Button></Link>
-              <Link href={`/dashboard/users/${user.id}`} passHref><Button color="secondary" variant="contained" size="small">Éditer</Button></Link>
+            <CardActions sx={{width:"10%", justifyContent: 'center'}}>
+              <Link href={`/dashboard/users/${user.id}/delete`} passHref><IconButton color="primary"><DeleteIcon /></IconButton></Link>
+              <Link href={`/dashboard/users/${user.id}`} passHref><IconButton color="secondary"><EditIcon /></IconButton></Link>
             </CardActions>
           </Card>
         )}
