98ba7a1c9da88a7647024d653ff5bec764a3f42d
HugCls <superhugh92@gmail.com>
2022-03-23 15:43:34 +0100

    artists/create better layout & responsive

--- src/pages/dashboard/artists/[id]/index.tsx
+++ src/pages/dashboard/artists/[id]/index.tsx
@@ -1,5 +1,15 @@
-import { Button, FormGroup, TextareaAutosize, TextField } from "@mui/material";
-import { Box } from "@mui/system";
+import {
+  Button,
+  ButtonBase,
+  Container,
+  FormGroup,
+  Grid,
+  styled,
+  TextareaAutosize,
+  TextField,
+  Theme,
+} from "@mui/material";
+import { Box, SxProps } from "@mui/system";
 import { Artist } from "@prisma/client";
 import Dashboard from "components/dashboard/LayoutDashboard";
 import Typography from "components/Typography";
@@ -9,13 +19,13 @@ import { GetServerSideProps } from "next";
 import { ParsedUrlQuery } from "querystring";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import Router from 'next/router'
+import Router from "next/router";
 
 interface IParams extends ParsedUrlQuery {
   id: string;
 }
 
-const ArtistDashboard = ({ artist}: {artist: Artist;}) => {
+const ArtistDashboard = ({ artist }: { artist: Artist }) => {
   const router = Router;
 
   const {
@@ -32,7 +42,6 @@ const ArtistDashboard = ({ artist}: {artist: Artist;}) => {
       email: artist.email,
       facebook_link: artist.facebook_link,
       instagram_link: artist.instagram_link,
-      
     },
   });
 
@@ -41,16 +50,23 @@ const ArtistDashboard = ({ artist}: {artist: Artist;}) => {
     "PUT"
   );
 
-
-  useEffect(()=> {
-    if (isLoading === false && apiData !== null)
-    {router.push('/dashboard/artists')}
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoading])
-
+  useEffect(() => {
+    if (isLoading === false && apiData !== null) {
+      router.push("/dashboard/artists");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading]);
 
   const onSubmit = async (data: Artist) => {
     request(data);
+  };
+
+  const item: SxProps<Theme> = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-around",
+    px: 5,
   };
 
   return (
@@ -58,79 +74,85 @@ const ArtistDashboard = ({ artist}: {artist: Artist;}) => {
       <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
         <input type="hidden" {...register("id")} />
         <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-around",
-            width: "100%",
-            marginTop: 5,
-          }}
-        >
-           <FormGroup
-            sx={{ display: "flex", flexDirection: "column", width: "45%" }}
-          >
-            <Typography variant="h4">Informations</Typography>
-            <TextField
-              label="Prénom"
-              variant="filled"
-              focused
-              {...register("firstname")}
-              sx={{ marginTop: 3 }}
-            />
-             <TextField
-              label="Nom"
-              variant="filled"
-              focused
-              {...register("lastname")}
-              sx={{ marginTop: 3 }}
-            />
-            <TextField
-              label="Courriel"
-              variant="filled"
-              focused
-              {...register("email")}
-              sx={{ marginTop: 3 }}
-            />
-             <TextField
-              label="Facebook"
-              variant="filled"
-              focused
-              {...register("facebook_link")}
-              sx={{ marginTop: 3 }}
-            />
-             <TextField
-              label="Instagram"
-              variant="filled"
-              focused
-              {...register("instagram_link")}
-              sx={{ marginTop: 3 }}
-            />
-          </FormGroup>
-          <FormGroup
-            sx={{ display: "flex", flexDirection: "column", width: "45%" }}
-          >
-            <Typography variant="h4">Biography</Typography>
-            <TextareaAutosize
-              aria-label="Biographie"
-              minRows={20}
-              placeholder=""
-              style={{ width: "100%" }}
-              {...register("biography")}
-            />
-          </FormGroup>
+      component="section"
+      sx={{mt: 25, mb: 8, display: 'flex', overflow: 'hidden' }}
+    >
+       <Container sx={{ display: 'flex', position: 'relative' }}>
+        <Grid container spacing={50}>
+          <Grid item xs={12} md={4}>
+            <Box sx={item}>
+              <FormGroup
+                sx={{ display: "flex", flexDirection: "column", width: "45%" }}
+              >
+                <Typography variant="h4" marked="center">Informations</Typography>
+
+                <TextField
+                  label="Prénom"
+                  variant="filled"
+                  focused
+                  {...register("firstname")}
+                  sx={{ marginTop: 3 }}
+                />
+                <TextField
+                  label="Nom"
+                  variant="filled"
+                  focused
+                  {...register("lastname")}
+                  sx={{ marginTop: 3 }}
+                />
+                <TextField
+                  label="Courriel"
+                  variant="filled"
+                  focused
+                  {...register("email")}
+                  sx={{ marginTop: 3 }}
+                />
+                <TextField
+                  label="Facebook"
+                  variant="filled"
+                  focused
+                  {...register("facebook_link")}
+                  sx={{ marginTop: 3 }}
+                />
+                <TextField
+                  label="Instagram"
+                  variant="filled"
+                  focused
+                  {...register("instagram_link")}
+                  sx={{ marginTop: 3 }}
+                />
+              </FormGroup>
+            </Box>
+          </Grid>
+        
+        <Grid item xs={12} md={4}>
+          <Box sx={item}>
+            <FormGroup
+              sx={{ display: "flex", flexDirection: "column", width: "80%" }}
+            >
+              <Typography  variant="h4" align='center' marked="center">Biographie</Typography>
+              <Box sx={{mt: 3}}>
+              <TextareaAutosize
+                aria-label="Biographie"
+                minRows={20}
+                placeholder=""
+                style={{minWidth: "150%", height: "115%" }}
+                {...register("biography")}
+              />
+              </Box>
+            </FormGroup>
+          </Box>
+        </Grid>
+        </Grid>
+        </Container>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-around",
-            width: "100%",
-            marginTop: 5,
-          }}
-        >
-          <Button color="secondary" variant="contained" type="submit">Enregistrer</Button>
+        
+
+        <Box sx={item}>
+          <Button color="secondary" variant="contained" type="submit">
+            Enregistrer
+          </Button>
+          
         </Box>
       </form>
     </Dashboard>
