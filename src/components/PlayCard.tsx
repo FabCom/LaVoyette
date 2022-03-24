7095ceb0be2c0192a0f77c20c8e3b626199915b8
HugCls <superhugh92@gmail.com>
2022-03-24 16:02:39 +0100

    lots of front improvement

--- src/components/PlayCard.tsx
+++ src/components/PlayCard.tsx
@@ -56,22 +56,8 @@ export default function PlayCard({ play }: { play: PlayWithAudienceAndTags }) {
                   <Chip key={i} label={categ.title} />;
                 })}
               </Stack>
-              {/* <ExpandMore
-                expand={expanded}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon /> */}
-              {/* </ExpandMore> */}
             </Box>
             <CardActions disableSpacing></CardActions>
-            {/* <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography variant="h5"></Typography>
-                <Typography variant="h5">{artist.biography}</Typography>
-              </CardContent>
-            </Collapse> */}
           </Box>
         </Grid>
         <Grid
@@ -80,16 +66,6 @@ export default function PlayCard({ play }: { play: PlayWithAudienceAndTags }) {
           md={30}
           sx={{ display: { md: "block", xs: "none" }, position: "relative" }}
         >
-          {/* <Box
-            sx={{
-              position: "absolute",
-              top: -67,
-              left: -67,
-              right: 0,
-              bottom: 0,
-              width: "100%",
-            }}
-          /> */}
           <Box
             component="img"
             src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.theatre-madeleine.com%2Fwp-content%2Fuploads%2F2019%2F10%2Ftheatre-de-la-madeleine-03.jpg&f=1&nofb=1"
