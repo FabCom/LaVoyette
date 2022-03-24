1339bcfe40769bec0c37f8b73030cbc244f94c11
HugCls <superhugh92@gmail.com>
2022-03-24 18:19:09 +0100

    dashboard/play/create better layout and responsive

--- src/pages/dashboard/plays/[id]/index.tsx
+++ src/pages/dashboard/plays/[id]/index.tsx
@@ -1,4 +1,4 @@
-import { Button, FormGroup, TextareaAutosize, TextField } from "@mui/material";
+import { Button, Container, FormGroup, Grid, TextareaAutosize, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import { AudienceCategory, Tag } from "@prisma/client";
 import Dashboard from "components/dashboard/LayoutDashboard";
@@ -9,163 +9,179 @@ import { GetServerSideProps } from "next";
 import { ParsedUrlQuery } from "querystring";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import Router from 'next/router'
+import Router from "next/router";
 
 type PlayWithAudienceAndTags = {
-	id: number;
-	title: string;
-	abstract: string;
-	duration: number;
-	audienceCategories: AudienceCategory[];
-	tags: Tag[];
+  id: number;
+  title: string;
+  abstract: string;
+  duration: number;
+  audienceCategories: AudienceCategory[];
+  tags: Tag[];
 };
 type RequestPlayWithAudienceAndTags = {
-	id: number;
-	title: string;
-	abstract: string;
-	duration: number;
-	audienceCategories: string;
-	tags: string;
+  id: number;
+  title: string;
+  abstract: string;
+  duration: number;
+  audienceCategories: string;
+  tags: string;
 };
 
 interface IParams extends ParsedUrlQuery {
-	id: string;
+  id: string;
 }
 
-const PlaysDashboard = ({ play }: { play: PlayWithAudienceAndTags; }) => {
-	const router = Router;
+const PlaysDashboard = ({ play }: { play: PlayWithAudienceAndTags }) => {
+  const router = Router;
 
-	const {
-		register,
-		setValue,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<RequestPlayWithAudienceAndTags>({
-		defaultValues: {
-			id: play.id,
-			title: play.title,
-			duration: play.duration,
-			abstract: play.abstract,
-			audienceCategories: play.audienceCategories.map(item => item.title).join(','),
-			tags: play.tags.map(item => item.title).join(','),
-		},
-	});
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RequestPlayWithAudienceAndTags>({
+    defaultValues: {
+      id: play.id,
+      title: play.title,
+      duration: play.duration,
+      abstract: play.abstract,
+      audienceCategories: play.audienceCategories
+        .map((item) => item.title)
+        .join(","),
+      tags: play.tags.map((item) => item.title).join(","),
+    },
+  });
 
-	const { isLoading, apiData, request } = useRequest<RequestPlayWithAudienceAndTags>(
-		`plays/${play.id}`,
-		"PUT"
-	);
+  const { isLoading, apiData, request } =
+    useRequest<RequestPlayWithAudienceAndTags>(`plays/${play.id}`, "PUT");
 
-
-	useEffect(() => {
-		if (isLoading === false && apiData !== null) { router.push('/dashboard/plays') }
+  useEffect(() => {
+    if (isLoading === false && apiData !== null) {
+      router.push("/dashboard/plays");
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [isLoading])
+  }, [isLoading]);
 
+  const onSubmit = async (data: RequestPlayWithAudienceAndTags) => {
+    console.log(data);
+    const requestData = {
+      title: data.title,
+      abstract: data.abstract,
+      duration: data.duration,
+      audienceCategories:
+        data.audienceCategories !== ""
+          ? data.audienceCategories.split(",").map((categ) => categ.trim())
+          : [],
+      tags:
+        data.tags !== ""
+          ? data.tags.split(",").map((categ) => categ.trim())
+          : [],
+    };
+    // console.log(requestData)
+    request(requestData);
+  };
 
-	const onSubmit = async (data: RequestPlayWithAudienceAndTags) => {
-		console.log(data)
-		const requestData = {
-			title: data.title,
-			abstract: data.abstract,
-			duration: data.duration,
-			audienceCategories: data.audienceCategories !== "" ? data.audienceCategories.split(",").map(categ => categ.trim()) : [],
-			tags: data.tags !== "" ? data.tags.split(",").map(categ => categ.trim()) : [],
-		};
-		// console.log(requestData)
-		request(requestData);
-	};
+  const item = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-around",
+    px: 5,
+  };
 
-	return (
-		<Dashboard>
-			<form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
-				<input type="hidden" {...register("id")} />
-				<Box
-					sx={{
-						display: "flex",
-						flexDirection: "row",
-						alignItems: "center",
-						justifyContent: "space-around",
-						width: "100%",
-						marginTop: 5,
-					}}
-				>
-					<FormGroup
-						sx={{ display: "flex", flexDirection: "column", width: "45%" }}
-					>
-						<Typography variant="h4">Informations</Typography>
-						<TextField
-							label="Titre"
-							variant="filled"
-							focused
-							{...register("title")}
-							sx={{ marginTop: 3 }}
-						/>
-						<TextField
-							label="Durée"
-							variant="filled"
-							focused
-							{...register("duration")}
-							sx={{ marginTop: 3 }}
-						/>
-						<TextField
-							label="Public"
-							variant="filled"
-							focused
-							{...register("audienceCategories")}
-							sx={{ marginTop: 3 }}
-						/>
+  return (
+    <Dashboard>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
+        <input type="hidden" {...register("id")} />
+        <Box
+          component="section"
+          sx={{ mt: 20, mb: 8, display: "flex", overflow: "hidden" }}
+        >
+		 <Container sx={{ display: "flex", position: "relative" }}>
+            <Grid container spacing={50}>
+              <Grid item xs={12} md={4}>
+                <Box sx={item}>	
+          <FormGroup
+            sx={{ display: "flex", flexDirection: "column", width: "45%" }}
+          >
+            <Typography variant="h4" marked="center">Informations</Typography>
+            <TextField
+              label="Titre"
+              variant="filled"
+              focused
+              {...register("title")}
+              sx={{ marginTop: 3 }}
+            />
+            <TextField
+              label="Durée"
+              variant="filled"
+              focused
+              {...register("duration")}
+              sx={{ marginTop: 3 }}
+            />
+            <TextField
+              label="Public"
+              variant="filled"
+              focused
+              {...register("audienceCategories")}
+              sx={{ marginTop: 3 }}
+            />
 
-						<TextField
-							label="Tag"
-							variant="filled"
-							focused
-							{...register("tags")}
-							sx={{ marginTop: 3 }}
-						/>
-					</FormGroup>
-					<FormGroup
-						sx={{ display: "flex", flexDirection: "column", width: "45%" }}
-					>
-						<Typography variant="h4">Description</Typography>
-						<TextareaAutosize
-							aria-label="abstract"
-							minRows={20}
-							placeholder=""
-							style={{ width: "100%" }}
-							{...register("abstract")}
-						/>
-					</FormGroup>
-				</Box>
-				<Box
-					sx={{
-						display: "flex",
-						flexDirection: "row",
-						alignItems: "center",
-						justifyContent: "space-around",
-						width: "100%",
-						marginTop: 5,
-					}}
-				>
-					<Button color="secondary" variant="contained" type="submit">Enregistrer</Button>
-				</Box>
-			</form>
-		</Dashboard>
-	);
+            <TextField
+              label="Tag"
+              variant="filled"
+              focused
+              {...register("tags")}
+              sx={{ marginTop: 3 }}
+            />
+          </FormGroup>
+		  </Box>
+              </Grid>
+
+              <Grid item xs={12} md={4}>
+                <Box sx={item}>
+          <FormGroup
+            sx={{ display: "flex", flexDirection: "column", width: "45%" }}
+          >
+            <Typography variant="h4" marked="center" >Description</Typography>
+			<Box sx={{ mt: 3 }} >
+            <TextareaAutosize
+              aria-label="abstract"
+              minRows={20}
+              placeholder=""
+              style={{ width: "150%", height: "100%" }}
+              {...register("abstract")}
+            />
+			</Box>
+          </FormGroup>
+        </Box>
+		</Grid>
+            </Grid>
+          </Container>
+        </Box>
+        <Box sx={item}>
+          <Button color="secondary" variant="contained" type="submit">
+            Enregistrer
+          </Button>
+        </Box>
+      </form>
+    </Dashboard>
+  );
 };
 
 export default PlaysDashboard;
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-	const { id } = params as IParams;
-	const play = await models.play.findUnique({
-		where: { id: parseInt(id) },
-		include: {
-			audienceCategories: { select: { title: true } },
-			tags: { select: { title: true } },
-		},
-	});
-	play?.audienceCategories.join(' ')
-	play?.tags.join(' ')
-	return { props: { play } };
+  const { id } = params as IParams;
+  const play = await models.play.findUnique({
+    where: { id: parseInt(id) },
+    include: {
+      audienceCategories: { select: { title: true } },
+      tags: { select: { title: true } },
+    },
+  });
+  play?.audienceCategories.join(" ");
+  play?.tags.join(" ");
+  return { props: { play } };
 };
