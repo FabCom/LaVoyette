9564b3128cb8085d52423e902deb6a898b92b485
Louis-Lorgere <louis.lorgere@gmail.com>
2022-03-24 17:35:46 +0100

    add profile avatar upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // lavoyette.s3.eu-west-3.amazonaws.com
   images: {
-    loader: 'imgix',
-    path: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.theatre-madeleine.com%2Fwp-content%2Fuploads%2F2019%2F10%2Ftheatre-de-la-madeleine-03.jpg&f=1&nofb=1',
+    domains: ['lavoyette.s3.eu-west-3.amazonaws.com', "external-content.duckduckgo.com"],
   },
+  // images: {
+  //   loader: 'imgix',
+  //   path: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.theatre-madeleine.com%2Fwp-content%2Fuploads%2F2019%2F10%2Ftheatre-de-la-madeleine-03.jpg&f=1&nofb=1',
+  // },
 }
 
 module.exports = nextConfig
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -114,7 +114,7 @@ const Navbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src="" />
               </IconButton>
             </Tooltip>
             <Menu
